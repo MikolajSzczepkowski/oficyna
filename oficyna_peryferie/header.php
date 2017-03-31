c2888eb336737cbfb1e9e2bc792c42a02c266dea
mikolajszczepkowski-coderslab <mikolajszczepkowski@gmail.com>
2017-04-01 00:15:11 +0200

    updates

--- oficyna_peryferie/header.php
+++ oficyna_peryferie/header.php
@@ -33,6 +33,17 @@
 		<link rel="apple-touch-icon" sizes="152x152" href="<?php echo $favicon_ipad_retina; ?>" />
 		<?php endif; ?>
 
+		<script>
+		 (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+		 (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+		 m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+		 })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+		 ga('create', 'UA-96560482-1', 'auto');
+		 ga('send', 'pageview');
+
+		</script>
+
 		<script type="text/javascript">
 			var isMobile = {
 					Android: function() {
